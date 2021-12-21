8189d588de2685367db271db346aee6f49b5fb8a
leandrocgsi <leandrocgsi@gmail.com>
2021-12-21 16:56:09 -0300

    Implementing Sort Strings in one Word

--- algorithms-playground/src/main/java/br/com/erudio/SortString.java
+++ algorithms-playground/src/main/java/br/com/erudio/SortString.java
@@ -3,25 +3,25 @@ package br.com.erudio;
 // https://www.geeksforgeeks.org/sort-a-string-in-java-2-different-ways/
 public class SortString {
 
-	public String sortString(String string) {
-		
-		char temp;
-		char arr[] = string.toCharArray(); 
-        int i = 0;
-        while (i <= arr.length) {
-            int j = i + 1;
-            while (j < arr.length) {
-                if (arr[j] < arr[i]) {
+public String sortString(String string) {
+	
+	char temp;
+	char arr[] = string.toCharArray(); 
+    int i = 0;
+    while (i <= arr.length) {
+        int j = i + 1;
+        while (j < arr.length) {
+            if (arr[j] < arr[i]) {
 
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-                j += 1;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
-            i += 1;
+            j += 1;
         }
-        //Arrays.sort(arr);
-		return new StringBuilder().append(arr).toString();
-	}
+        i += 1;
+    }
+    //Arrays.sort(arr);
+	return new StringBuilder().append(arr).toString();
+}
 }
